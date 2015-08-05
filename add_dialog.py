cc5ec9542f358216668614bddf45ad221e3a7f1c
Nattefrost <escande.d@gmail.com>
2015-08-05 18:39:37 +0200

    Fixed problem with the boolean checkbox on add_dialog.py

--- add_dialog.py
+++ add_dialog.py
@@ -7,6 +7,7 @@ import db_access
 class AddDialog:
     def __init__(self, parent):
         self.root = tk.Tk()
+        self.isReadVar = tk.BooleanVar()
         self.root.eval('tk::PlaceWindow %s center' % self.root.winfo_pathname(self.root.winfo_id()))
         self.parent = parent
         self.root.title("Add a book")
@@ -14,19 +15,18 @@ class AddDialog:
         self.result = None
         self.style = ttk.Style()
         self.style.theme_use('clam')
-
         # StringVars to add a book
         self.titleVar = tk.StringVar()
         self.authorVar = tk.StringVar()
         self.collectionVar = tk.StringVar()
-        self.isReadVar = tk.IntVar()
+
 
         # Entries to add a book
 
         self.title_entry = ttk.Entry(self.root,textvariable=self.titleVar,width=15)
         self.author_entry = ttk.Entry(self.root,textvariable=self.authorVar,width=15)
         self.collection_entry = ttk.Entry(self.root,textvariable=self.collectionVar, width=15)
-        self.isRead_check = ttk.Checkbutton(self.root,text="READ?",width=6,variable=self.isReadVar)
+        self.isRead_check = tk.Checkbutton(self.root,text="READ?",width=7,variable=self.isReadVar,command=self.change_bool)
 
         self.title_entry.grid(column=0,row=0)
         self.author_entry.grid(column=0,row=1)
@@ -48,12 +48,19 @@ class AddDialog:
 
         self.root.mainloop()
 
+    def change_bool(self):
+        if self.isReadVar.get() != True:
+            self.isReadVar.set(True)
+        else:
+            self.isReadVar.set(False)
+        print(self.isReadVar.get() )
+
     def cancel(self):
         self.root.destroy()
 
     def validate(self):
         db_access.add_book(self.title_entry.get().capitalize(), self.author_entry.get().capitalize(),
             self.collection_entry.get().capitalize() , self.isReadVar.get() )
-        self.parent.tree_data = db_access.get_books_to_view()
+        #self.parent.tree_data = db_access.get_books_to_view()
         self.parent.load_all_callback()
         self.root.destroy()
