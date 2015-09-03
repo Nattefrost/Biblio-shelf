3beb30e0ce79452ef397171d8478638bc51c952c
Nattefrost <escande.d@gmail.com>
2015-09-03 21:24:15 +0200

    made setup script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 
 import sys
-
+"""
 base = None
 if sys.platform == 'win32':
     base = 'Win32GUI'
@@ -19,3 +19,22 @@ setup(name='Biblio',
       executables=executables
       )
 
+"""
+
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = "Biblio",
+        version = "0.2",
+        description = "Manage your personal books with ease",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("main.py", base=base)])
